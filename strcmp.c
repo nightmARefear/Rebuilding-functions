ea1b40bcb2b9a84ac095e2855bd8d500dd6dee09
nightmARefear <alexander_v2000@windowslive.com>
2019-05-12 05:22:01 +0300

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,56 @@
+// rebuilding various functions from the string.h library
+
+#include <stdio.h>
+
+int strlen(unsigned const char *str)
+{
+    int length = 0;
+
+    while (str[length] != '\0')
+        length++;
+    return length;
+};
+
+int strcmp(unsigned const char *str1, unsigned const char *str2) // if str1 == str2 return 0, if str1 < str2 return 1, if str1 > str2 return -1
+{
+    int i = 0, counter = 0, counter1 = 0, counter2 = 0;
+
+    while (str1[i] != '\0' && str2[i] != '\0') 
+    {
+        if (str1[i] == str2[i])
+            counter++; // counter1++; counter2++;
+        else if (str1[i] > str2[i])
+            counter1++;
+        else
+            counter2++;
+        i++;
+    }
+
+    if (counter == strlen(str1) && counter == strlen(str2)) // counter1 == counter2
+        return 0;                                           // the strings are equal
+    else if (counter1 > counter2)
+        return -1; // the first string is bigger than the second
+    else
+        return 1; // the second string is bigger than the first
+};
+
+int main()
+{
+    // testing the functions
+    char name1[30], name2[30];
+
+    printf("\n Please enter your name: ");
+    fgets(name1, sizeof(name1), stdin);
+
+    printf("\n Please enter your name: ");
+    fgets(name2, sizeof(name2), stdin);
+
+    if (strcmp(name1, name2) == 0)
+        printf("\n The names are equal! \n");
+    else if (strcmp(name1, name2) == -1)
+        printf("\n The first name is bigger than the second! \n");
+    else
+        printf("\n The second name is bigger than the first! \n");
+
+    return 0;
+}
