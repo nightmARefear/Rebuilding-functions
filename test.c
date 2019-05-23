e20fc2c32cbee837c7e5c6d17b49958e2ddb06cb
nightmARefear <48566860+nightmARefear@users.noreply.github.com>
2019-05-23 13:38:15 +0300

    Update test.c
    updated the test.c file to match the exact name of the functions in the header file

--- test.c
+++ test.c
@@ -1,7 +1,6 @@
 /* tests the functions */
 #include <stdio.h>
-#include <strlen.h>
-#include <strcmp.h>
+#include "string2.h"
 
 int main()
 {
@@ -14,12 +13,25 @@ int main()
     fgets(name2, sizeof(name2), stdin);
 
     // testing the functions
-    if (strcmp(name1, name2) == 0)
+    if (strcmp2(name1, name2) == 0)
         printf("\n The names are equal! \n");
-    else if (strcmp(name1, name2) == -1)
+    else if (strcmp2(name1, name2) == -1)
         printf("\n The first name is bigger than the second! \n");
     else
         printf("\n The second name is bigger than the first! \n");
 
+	strcpy2(name1, name2);
+	printf("\n name1 = %s \n", name1);
+
+	char First_Name[30], Last_Name[30];
+
+	printf("\n Enter your first and last name: \n");
+	scanf("%s %s", First_Name, Last_Name);
+
+	strcat2(First_Name, Last_Name);
+
+	printf("\n Full name: %s \n", First_Name);
+
+	system("pause"); // pauses the terminal
     return 0;
 }
