0b5ec5fc88c9ac5ad226d8be2196c686fce74b43
nightmARefear <alexander_v2000@windowslive.com>
2019-06-03 22:51:52 +0300

    Update ctype.h

--- ctype.h
+++ ctype.h
@@ -1,7 +1,47 @@
 int isdigit(unsigned const char target)
 {
     if (target >= 0 && target <= 9)
+        return 1;
+    else
         return 0;
+}
+
+int isalpha(unsigned const char target)
+{
+    if ((target >= 'a' && target <= 'z') || (target >= 'A' && target <= 'Z'))
+        return 1;
+    else
+        return 0;
+}
+
+int isdigit(unsigned const char target)
+{
+    if (target >= 0 && target <= 9)
+        return 1;
+    else
+        return 0;
+}
+
+int isalnum(unsigned const char target)
+{
+    if (isalpha(target) || isdigit(target))
+        return 1;
+    else
+        return 0;
+}
+
+int islower(unsigned const char target)
+{
+    if (target >= 'a' && target <= 'z')
+        return 1;
     else 
-        return 1;    
+        return 0;    
+}
+
+int isupper(unsigned const char target)
+{
+    if (target >= 'A' && target <= 'Z')
+        return 1;
+    else 
+        return 0;    
 }
